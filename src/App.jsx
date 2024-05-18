66cde8202b7d867fc8865454323da2a866f2d815
nomanshakib <nomanshakib.official@gmail.com>
2024-05-18 22:04:08 +0600

    project set up with vite,react,Tailwind CSS, daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-red-700 text-center">
+        Price Options
+      </h1>
+    </>
+  );
+}
+
+export default App
